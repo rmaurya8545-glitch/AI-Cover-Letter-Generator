1a052253a3cef2b3ce7c81ca846bb77a2a2d5ab4
Roshani Maurya <rmaurya8545@gmail.com>
2026-08-09 13:00:15 +0530

    api.js ko correct kiya

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,5 +1,5 @@
 export async function generateCoverLetter(formData){
-    const res = await fetch('http://localhost:3000/api/generate',{
+    const res = await fetch('https://ai-cover-letter-generator-ceqg.onrender.com/api/generate',{
         method:'POST',
         headers:{ 'Content-Type':'application/json'},
         body:JSON.stringify(formData)
